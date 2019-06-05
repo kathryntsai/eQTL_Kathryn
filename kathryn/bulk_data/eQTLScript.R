55fb734ddef75ffc4a853bd4fa65873ccc90556b
Katie <katctsai@gmail.com>
2019-06-05 18:21:13 -0400

    changed source of pfizer data from rda to supplementary, added beginning of question 3

--- kathryn/bulk_data/eQTLScript.R
+++ kathryn/bulk_data/eQTLScript.R
@@ -10,16 +10,17 @@
 library(data.table)
 library(dplyr)
 load("files_for_pfizer_eqtl.rda")
+davenport_data <- read.csv("pfizer_eqtl_table_1.csv")
 franke_cis_data <- fread("cis-eQTL_significant_20181017.txt", header = TRUE, sep = "\t", dec = ".")
 
 # inner_join (only keep observations that are similar) by SNPS+GENE
 franke_cis_gene_snps <- unique(data.table(franke_cis_data[,8], franke_cis_data[,2])) #10,527,168x2
-davenport_gene_snps <- data.table(sub("*\\.[0-9]", "", pairs.int.tss[,1]), pairs.int.tss[,2]) #4976x2
+davenport_gene_snps <- data.table(sub("*\\.[0-9]", "", davenport_data[,"Ensembl_ID"]), davenport_data[,"SNP"]) #4818x2
 colnames(davenport_gene_snps) <- c("Gene", "SNP")
-franke_davenport_gene_snps_common <- inner_join(davenport_gene_snps, franke_cis_gene_snps) #2887
+franke_davenport_gene_snps_common <- inner_join(davenport_gene_snps, franke_cis_gene_snps) #2962
 # proportions:
-nrow(franke_davenport_gene_snps_common)/nrow(davenport_gene_snps) # = 58.01849% proportion!
-nrow(franke_davenport_gene_snps_common)/nrow(franke_cis_gene_snps) # = 0.0002742428 proportion!
+nrow(franke_davenport_gene_snps_common)/nrow(davenport_gene_snps) # = 61.47779% proportion!
+nrow(franke_davenport_gene_snps_common)/nrow(franke_cis_gene_snps) # = 0.02813672% proportion!
 
 # ==========================================================
 # QUESTION 2
@@ -36,5 +37,14 @@ franke_cis_trans_common <- inner_join(franke_trans_snps, franke_cis_snps, by="SN
 nrow(franke_cis_trans_common)/nrow(franke_trans_snps) # 85.15443% proportion!
 nrow(franke_cis_trans_common)/nrow(franke_cis_snps) # 0.08867992% proportion!
 
+# ==========================================================
+# QUESTION 3
+# ==========================================================
+# What % of eQTLs (cis / trans consider separately) overlap 
+# a motif or are in LD with a SNP that overlaps a motif? 
+# If a SNP is not genotyped or poorly imputed (missing data or incorrect data), 
+# we won’t be able to see it’s true association with gene expression. 
+# Therefore, a SNP in tight LD with this poorly documented SNP (which is in reality the causal SNP) 
+# might show an artificially strong association signal with expression. 
 
   

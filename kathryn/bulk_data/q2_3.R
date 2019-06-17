24b867a2a4f517c9a8b89259a2ad65a3794cd20c
Katie <katctsai@gmail.com>
2019-06-17 16:52:14 -0400

    Update q2_3.R

--- kathryn/bulk_data/q2_3.R
+++ kathryn/bulk_data/q2_3.R
@@ -1,7 +1,7 @@
+# UNFINISHED
 # ==========================================================
 # QUESTION 2_3
 # ==========================================================
-# UNFINISHED
 #Search for motif in sequence with swapped alleles (according to cis eQTL ref/alt).: swap these? franke_cis_data[,"AssessedAllele"], franke_cis_data[,"OtherAllele"]
 
 # http://www.eqtlgen.org/cis-eqtls.html
@@ -143,6 +143,8 @@ library("Biostrings")
 # Papers from Friday 6/14/19
 # https://string-db.org/cgi/network.pl?taskId=BtryuxaMZp8F Protein-Protein Interaction - search trios for correlation between protein-protein interaction score of SNP with cis EQTL gene --> making protein product of same name, and see if he protein has a high score with trans gene that also has a relationship to the trans SNP
 # TF comes adn binds from Regime A, and then maybe something else interacts with binding, and possibly feedback loop
+
+# Find Hi-C data: look at HiGlass or some other dataset// don't use Hi-C data for T-cell or B-cell data specifically
 # https://bioconductor.org/packages/release/bioc/html/STRINGdb.html
 # http://regulatorycircuits.org/index.html
 # https://www.nature.com/articles/nmeth.3799

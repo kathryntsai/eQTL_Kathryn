e4e022c7c3a4c95219db3ca99f19bea887d269cd
Katie <katctsai@gmail.com>
2019-06-25 07:49:08 -0400

    updates from running code for q2_3

--- kathryn/bulk_data/q2_3_redo.R
+++ kathryn/bulk_data/q2_3_redo.R
@@ -2,23 +2,22 @@
 # QUESTION 2_3_Redo
 # ==========================================================
 
-a <- franke_cis_data[which(Gene==c('ENSG00000127191', 
-                                   'ENSG00000214279', 
-                                   'ENSG00000168765', 
-                                   'ENSG00000125966',
-                                   'ENSG00000258531',
-                                   'ENSG00000237437'
-)), c('Gene', 'GeneSymbol', 'GeneChr', 'GenePos')]
+a <- franke_cis_data[which(GeneChr==1), c('Gene', 'GeneSymbol', 'GeneChr', 'GenePos')]
 a <- a[!duplicated(a),]
 
-a_gene_snp <- franke_cis_data[which(Gene==c('ENSG00000127191', 
-                                            'ENSG00000214279', 
-                                            'ENSG00000168765', 
-                                            'ENSG00000125966',
-                                            'ENSG00000258531',
-                                            'ENSG00000237437'
-)), c('Gene', 'GeneSymbol', 'GeneChr', 'GenePos', 'SNP', 'SNPChr', 'SNPPos')]
-a_gene <- a_gene_snp[!duplicated(a_gene_snp$Gene),]
+a_gene_snp <- franke_cis_data[which(GeneChr==1), c('Gene', 'GeneSymbol', 'GeneChr', 'GenePos', 'SNP', 'SNPChr', 'SNPPos')]
+# check
+# a_gene <- a_gene_snp[!duplicated(a_gene_snp$Gene),]
 
-# read in chromosomes from hg19 genome, taken from homer/data/genomes/hg19
-chr1 <- read.table('q2_3_input/chr1.fa')
+# read in chromosomes from hg19 genome, taken from homer/data/genomes/hg19 from q2_3_input folder
+chr1 <- fread("q2_3_input/chr1.fa")
+chr1_collapsed <- gsub(', |\"|\n', "", paste(chr1,collapse=""))
+fwrite(as.list(chr1_collapsed), "chr1_character.txt")
+
+a_gene_snp$SNPWindowStart <- a_gene_snp$SNPPos-20
+a_gene_snp$SNPWindowEnd <- a_gene_snp$SNPPos+20
+
+# ryan reynolds should've played gatsby
+# jodie switched labs
+
+HiC_data <- fread("/Users/kathryntsai/OneDrive\ -\ Villanova\ University/College/2018-2019/Summer\ 2019/TFs_eQTLs_Research/Important_Files/PeripheralBloodHiC/GSM3612257_Neutrophil_Ecoli_HiC_rep1_validPairs.txt")

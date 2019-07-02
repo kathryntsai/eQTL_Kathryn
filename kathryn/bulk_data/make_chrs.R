511aff0b1b62d749e105e160f413d8ac5c134c0f
Katie <katctsai@gmail.com>
2019-07-02 12:08:41 -0400

    Make_chrs.R modified, try to run it on the cluster

--- kathryn/bulk_data/make_chrs.R
+++ kathryn/bulk_data/make_chrs.R
@@ -2,10 +2,10 @@
 # Load full chr1
 # ==========================================================
 
-full_chr1 <- readRDS("q4_input/full_chr1.rds")
-full_chr1_mod <- gsub('c\\(|\\)', "", full_chr1)
-full_chr1_mod <- gsub(')', "", full_chr1_mod)
-loadRDS(full_chr1_mod, "hypothesis1_output/full_chr1.rds")
+# full_chr1 <- readRDS("q4_input/full_chr1.rds")
+# full_chr1_mod <- gsub('c\\(|\\)', "", full_chr1)
+# full_chr1_mod <- gsub(')', "", full_chr1_mod)
+# saveRDS(full_chr1_mod, "hypothesis1_output/full_chr1.rds")
 
 # x <- 4985013
 # div <- seq_len(x)
@@ -18,152 +18,197 @@ loadRDS(full_chr1_mod, "hypothesis1_output/full_chr1.rds")
 
 # read in chromosomes from hg19 genome, taken from homer/data/genomes/hg19 from q2_3_input folder
 chr2 <- fread("q4_input/chr2.fa")
-div <- seq_len(nchar(chr2))
-chr2_factors <- paste(div[x %% div == 0], collapse=", ")
+# div <- seq_len(nchar(chr2))
+# chr2_factors <- paste(div[x %% div == 0], collapse=", ")
 
 chr3 <- fread("q4_input/chr3.fa")
-div <- seq_len(nchar(chr3))
-chr3_factors <- paste(div[x %% div == 0], collapse=", ")
+# div <- seq_len(nchar(chr3))
+# chr3_factors <- paste(div[x %% div == 0], collapse=", ")
 
 chr4 <- fread("q4_input/chr4.fa")
-div <- seq_len(nchar(chr4))
-chr4_factors <- paste(div[x %% div == 0], collapse=", ")
+# div <- seq_len(nchar(chr4))
+# chr4_factors <- paste(div[x %% div == 0], collapse=", ")
 
 chr5 <- fread("q4_input/chr5.fa")
-div <- seq_len(nchar(chr5))
-chr5_factors <- paste(div[x %% div == 0], collapse=", ")
+# div <- seq_len(nchar(chr5))
+# chr5_factors <- paste(div[x %% div == 0], collapse=", ")
 
 chr6 <- fread("q4_input/chr6.fa")
-div <- seq_len(nchar(chr6))
-chr6_factors <- paste(div[x %% div == 0], collapse=", ")
+# div <- seq_len(nchar(chr6))
+# chr6_factors <- paste(div[x %% div == 0], collapse=", ")
 
 chr7 <- fread("q4_input/chr7.fa")
-div <- seq_len(nchar(chr7))
-chr7_factors <- paste(div[x %% div == 0], collapse=", ")
+# div <- seq_len(nchar(chr7))
+# chr7_factors <- paste(div[x %% div == 0], collapse=", ")
 
 chr8 <- fread("q4_input/chr8.fa")
-div <- seq_len(nchar(chr8))
-chr8_factors <- paste(div[x %% div == 0], collapse=", ")
+# div <- seq_len(nchar(chr8))
+# chr8_factors <- paste(div[x %% div == 0], collapse=", ")
 
 chr9 <- fread("q4_input/chr9.fa")
-div <- seq_len(nchar(chr9))
-chr9_factors <- paste(div[x %% div == 0], collapse=", ")
+# div <- seq_len(nchar(chr9))
+# chr9_factors <- paste(div[x %% div == 0], collapse=", ")
 
 chr10 <- fread("q4_input/chr10.fa")
-div <- seq_len(nchar(chr10))
-chr10_factors <- paste(div[x %% div == 0], collapse=", ")
+# div <- seq_len(nchar(chr10))
+# chr10_factors <- paste(div[x %% div == 0], collapse=", ")
 
 chr11 <- fread("q4_input/chr11.fa")
-div <- seq_len(nchar(chr11))
-chr11_factors <- paste(div[x %% div == 0], collapse=", ")
+# div <- seq_len(nchar(chr11))
+# chr11_factors <- paste(div[x %% div == 0], collapse=", ")
 
 chr12 <- fread("q4_input/chr12.fa")
-div <- seq_len(nchar(chr12))
-chr12_factors <- paste(div[x %% div == 0], collapse=", ")
+# div <- seq_len(nchar(chr12))
+# chr12_factors <- paste(div[x %% div == 0], collapse=", ")
 
 chr13 <- fread("q4_input/chr13.fa")
-div <- seq_len(nchar(chr13))
-chr13_factors <- paste(div[x %% div == 0], collapse=", ")
+# div <- seq_len(nchar(chr13))
+# chr13_factors <- paste(div[x %% div == 0], collapse=", ")
 
 chr14 <- fread("q4_input/chr14.fa")
-div <- seq_len(nchar(chr14))
-chr14_factors <- paste(div[x %% div == 0], collapse=", ")
+# div <- seq_len(nchar(chr14))
+# chr14_factors <- paste(div[x %% div == 0], collapse=", ")
 
 chr15 <- fread("q4_input/chr15.fa")
-div <- seq_len(nchar(chr15))
-chr15_factors <- paste(div[x %% div == 0], collapse=", ")
+# div <- seq_len(nchar(chr15))
+# chr15_factors <- paste(div[x %% div == 0], collapse=", ")
 
 chr16 <- fread("q4_input/chr16.fa")
-div <- seq_len(nchar(chr16))
-chr16_factors <- paste(div[x %% div == 0], collapse=", ")
+# div <- seq_len(nchar(chr16))
+# chr16_factors <- paste(div[x %% div == 0], collapse=", ")
 
 chr17 <- fread("q4_input/chr17.fa")
-div <- seq_len(nchar(chr17))
-chr17_factors <- paste(div[x %% div == 0], collapse=", ")
+# div <- seq_len(nchar(chr17))
+# chr17_factors <- paste(div[x %% div == 0], collapse=", ")
 
 chr18 <- fread("q4_input/chr18.fa")
-div <- seq_len(nchar(chr18))
-chr18_factors <- paste(div[x %% div == 0], collapse=", ")
+# div <- seq_len(nchar(chr18))
+# chr18_factors <- paste(div[x %% div == 0], collapse=", ")
 
 chr19 <- fread("q4_input/chr19.fa")
-div <- seq_len(nchar(chr19))
-chr19_factors <- paste(div[x %% div == 0], collapse=", ")
+# div <- seq_len(nchar(chr19))
+# chr19_factors <- paste(div[x %% div == 0], collapse=", ")
 
 chr20 <- fread("q4_input/chr20.fa")
-div <- seq_len(nchar(chr20))
-chr20_factors <- paste(div[x %% div == 0], collapse=", ")
+# div <- seq_len(nchar(chr20))
+# chr20_factors <- paste(div[x %% div == 0], collapse=", ")
 
 chr21 <- fread("q4_input/chr21.fa")
-div <- seq_len(nchar(chr21))
-chr21_factors <- paste(div[x %% div == 0], collapse=", ")
+# div <- seq_len(nchar(chr21))
+# chr21_factors <- paste(div[x %% div == 0], collapse=", ")
 
 chr22 <- fread("q4_input/chr22.fa")
-div <- seq_len(nchar(chr22))
-chr22_factors <- paste(div[x %% div == 0], collapse=", ")
+# div <- seq_len(nchar(chr22))
+# chr22_factors <- paste(div[x %% div == 0], collapse=", ")
 
 # # ==========================================================
-# # Create Chr1 Information
+# # Create Information Part 1
 # # ==========================================================
 # 
-# # subset chr1
-# chosen_factor=171897 # need 29 arrays of 171897 rows of 50 chars each
-# counter=1
-# total_rows <- nrow(chr1) # 4985013
-# x <- matrix(0, nrow=total_rows/chosen_factor*2-2, ncol=1)
-# for (i in 1:nrow(x)){
-#   #print(i)
-#   if (i %% 2 == 1){
-#     x[i,1] <- chosen_factor * counter 
-#   }
-#   else{
-#     x[i,1] <- chosen_factor * counter + 1
-#     counter = counter + 1
-#   }
-# }
-# 
-# each
-chr1 <- gsub(', |\"|\n', "", paste(chr1[1:x[1]],collapse="")) #should be 171897*50=8594850, then nchar(chr1_1) = 8594853
-chr2 <- gsub(', |\"|\n', "", paste(chr1[x[2]:x[3]],collapse=""))
-chr3 <- gsub(', |\"|\n', "", paste(chr1[x[4]:x[5]],collapse=""))
-chr4 <- gsub(', |\"|\n', "", paste(chr1[x[6]:x[7]],collapse=""))
-chr5 <- gsub(', |\"|\n', "", paste(chr1[x[8]:x[9]],collapse=""))
-chr6 <- gsub(', |\"|\n', "", paste(chr1[x[10]:x[11]],collapse=""))
-chr7 <- gsub(', |\"|\n', "", paste(chr1[x[12]:x[13]],collapse=""))
-chr8 <- gsub(', |\"|\n', "", paste(chr1[x[14]:x[15]],collapse=""))
-chr9 <- gsub(', |\"|\n', "", paste(chr1[x[16]:x[17]],collapse=""))
-chr10 <- gsub(', |\"|\n', "", paste(chr1[x[18]:x[19]],collapse=""))
-chr11 <- gsub(', |\"|\n', "", paste(chr1[x[20]:x[21]],collapse=""))
-chr12 <- gsub(', |\"|\n', "", paste(chr1[x[22]:x[23]],collapse=""))
-chr13 <- gsub(', |\"|\n', "", paste(chr1[x[24]:x[25]],collapse=""))
-chr14 <- gsub(', |\"|\n', "", paste(chr1[x[26]:x[27]],collapse=""))
-chr15 <- gsub(', |\"|\n', "", paste(chr1[x[28]:x[29]],collapse=""))
-chr16 <- gsub(', |\"|\n', "", paste(chr1[x[30]:x[31]],collapse=""))
-chr17 <- gsub(', |\"|\n', "", paste(chr1[x[32]:x[33]],collapse=""))
-chr18 <- gsub(', |\"|\n', "", paste(chr1[x[34]:x[35]],collapse=""))
-chr19 <- gsub(', |\"|\n', "", paste(chr1[x[36]:x[37]],collapse=""))
-chr20 <- gsub(', |\"|\n', "", paste(chr1[x[38]:x[39]],collapse=""))
-chr21 <- gsub(', |\"|\n', "", paste(chr1[x[40]:x[41]],collapse=""))
-chr22 <- gsub(', |\"|\n', "", paste(chr1[x[42]:x[43]],collapse=""))
+chr2 <- gsub(', |\"|\n', "", chr2)
+full_chr2 <- gsub('c\\(|\\)', "", full_chr2)
+full_chr2 <- gsub(')', "", full_chr2)
 
-saveRDS(chr1, 'q2_3_output/chr1.rds')  
-saveRDS(chr2, 'q2_3_output/chr2.rds')  
-saveRDS(chr3, 'q2_3_output/chr3.rds')  
-saveRDS(chr4, 'q2_3_output/chr4.rds')  
-saveRDS(chr5, 'q2_3_output/chr5.rds')  
-saveRDS(chr6, 'q2_3_output/chr6.rds')  
-saveRDS(chr7, 'q2_3_output/chr7.rds')  
-saveRDS(chr8, 'q2_3_output/chr8.rds')  
-saveRDS(chr9, 'q2_3_output/chr9.rds')  
-saveRDS(chr10, 'q2_3_output/chr10.rds')  
-saveRDS(chr11, 'q2_3_output/chr11.rds')  
-saveRDS(chr12, 'q2_3_output/chr12.rds')  
-saveRDS(chr13, 'q2_3_output/chr13.rds')  
-saveRDS(chr14, 'q2_3_output/chr14.rds')  
-saveRDS(chr15, 'q2_3_output/chr15.rds')  
-saveRDS(chr16, 'q2_3_output/chr16.rds')  
-saveRDS(chr17, 'q2_3_output/chr17.rds')  
-saveRDS(chr18, 'q2_3_output/chr18.rds')  
-saveRDS(chr19, 'q2_3_output/chr19.rds')  
-saveRDS(chr20, 'q2_3_output/chr20.rds')  
-saveRDS(chr21, 'q2_3_output/chr21.rds')  
-saveRDS(chr22, 'q2_3_output/chr22.rds')  
+chr3 <- gsub(', |\"|\n', "", chr3)
+full_chr3 <- gsub('c\\(|\\)', "", full_chr3)
+full_chr3 <- gsub(')', "", full_chr3)
+
+chr4 <- gsub(', |\"|\n', "", chr4)
+full_chr4 <- gsub('c\\(|\\)', "", full_chr4)
+full_chr4 <- gsub(')', "", full_chr4)
+
+chr5 <- gsub(', |\"|\n', "", chr5)
+full_chr5 <- gsub('c\\(|\\)', "", full_chr5)
+full_chr5 <- gsub(')', "", full_chr5)
+
+chr6 <- gsub(', |\"|\n', "", chr6)
+full_chr6 <- gsub('c\\(|\\)', "", full_chr6)
+full_chr6 <- gsub(')', "", full_chr6)
+
+chr7 <- gsub(', |\"|\n', "", chr7)
+full_chr7 <- gsub('c\\(|\\)', "", full_chr7)
+full_chr7 <- gsub(')', "", full_chr7)
+
+chr8 <- gsub(', |\"|\n', "", chr8)
+full_chr8 <- gsub('c\\(|\\)', "", full_chr8)
+full_chr8 <- gsub(')', "", full_chr8)
+
+chr9 <- gsub(', |\"|\n', "", chr9)
+full_chr9 <- gsub('c\\(|\\)', "", full_chr9)
+full_chr9 <- gsub(')', "", full_chr9)
+
+chr10 <- gsub(', |\"|\n', "", chr10)
+full_chr10 <- gsub('c\\(|\\)', "", full_chr10)
+full_chr10 <- gsub(')', "", full_chr10)
+
+chr11 <- gsub(', |\"|\n', "", chr11)
+full_chr11 <- gsub('c\\(|\\)', "", full_chr11)
+full_chr11 <- gsub(')', "", full_chr11)
+
+chr12 <- gsub(', |\"|\n', "", chr12)
+full_chr12 <- gsub('c\\(|\\)', "", full_chr12)
+full_chr12 <- gsub(')', "", full_chr12)
+
+chr13 <- gsub(', |\"|\n', "", chr13)
+full_chr13 <- gsub('c\\(|\\)', "", full_chr13)
+full_chr13 <- gsub(')', "", full_chr13)
+
+chr14 <- gsub(', |\"|\n', "", chr14)
+full_chr14 <- gsub('c\\(|\\)', "", full_chr14)
+full_chr14 <- gsub(')', "", full_chr14)
+
+chr15 <- gsub(', |\"|\n', "", chr15)
+full_chr15 <- gsub('c\\(|\\)', "", full_chr15)
+full_chr15 <- gsub(')', "", full_chr15)
+
+chr16 <- gsub(', |\"|\n', "", chr16)
+full_chr16 <- gsub('c\\(|\\)', "", full_chr16)
+full_chr16 <- gsub(')', "", full_chr16)
+
+chr17 <- gsub(', |\"|\n', "", chr17)
+full_chr17 <- gsub('c\\(|\\)', "", full_chr17)
+full_chr17 <- gsub(')', "", full_chr17)
+
+chr18 <- gsub(', |\"|\n', "", chr18)
+full_chr18 <- gsub('c\\(|\\)', "", full_chr18)
+full_chr18 <- gsub(')', "", full_chr18)
+
+chr19 <- gsub(', |\"|\n', "", chr19)
+full_chr19 <- gsub('c\\(|\\)', "", full_chr19)
+full_chr19 <- gsub(')', "", full_chr19)
+
+chr20 <- gsub(', |\"|\n', "", chr20)
+full_chr20 <- gsub('c\\(|\\)', "", full_chr20)
+full_chr20 <- gsub(')', "", full_chr20)
+
+chr21 <- gsub(', |\"|\n', "", chr21)
+full_chr21 <- gsub('c\\(|\\)', "", full_chr21)
+full_chr21 <- gsub(')', "", full_chr21)
+
+chr22 <- gsub(', |\"|\n', "", chr22)
+full_chr22 <- gsub('c\\(|\\)', "", full_chr22)
+full_chr22 <- gsub(')', "", full_chr22)
+
+
+saveRDS(full_chr1, 'hypothesis1_output/full_chr1.rds')  
+saveRDS(full_chr2, 'hypothesis1_output/full_chr2.rds')  
+saveRDS(full_chr3, 'hypothesis1_output/full_chr3.rds')  
+saveRDS(full_chr4, 'hypothesis1_output/full_chr4.rds')  
+saveRDS(full_chr5, 'hypothesis1_output/full_chr5.rds')  
+saveRDS(full_chr6, 'hypothesis1_output/full_chr6.rds')  
+saveRDS(full_chr7, 'hypothesis1_output/full_chr7.rds')  
+saveRDS(full_chr8, 'hypothesis1_output/full_chr8.rds')  
+saveRDS(full_chr9, 'hypothesis1_output/full_chr9.rds')  
+saveRDS(full_chr10, 'hypothesis1_output/full_chr10.rds')  
+saveRDS(full_chr11, 'hypothesis1_output/full_chr11.rds')  
+saveRDS(full_chr12, 'hypothesis1_output/full_chr12.rds')  
+saveRDS(full_chr13, 'hypothesis1_output/full_chr13.rds')  
+saveRDS(full_chr14, 'hypothesis1_output/full_chr14.rds')  
+saveRDS(full_chr15, 'hypothesis1_output/full_chr15.rds')  
+saveRDS(full_chr16, 'hypothesis1_output/full_chr16.rds')  
+saveRDS(full_chr17, 'hypothesis1_output/full_chr17.rds')  
+saveRDS(full_chr18, 'hypothesis1_output/full_chr18.rds')  
+saveRDS(full_chr19, 'hypothesis1_output/full_chr19.rds')  
+saveRDS(full_chr20, 'hypothesis1_output/full_chr20.rds')  
+saveRDS(full_chr21, 'hypothesis1_output/full_chr21.rds')  
+saveRDS(full_chr22, 'hypothesis1_output/full_chr22.rds')  

3a9c933bf040cfdda978ac33c4e2389c665e7a37
Katie <katctsai@gmail.com>
2019-07-02 00:45:45 -0400

    added downsampled q4_output

--- kathryn/bulk_data/q4.R
+++ kathryn/bulk_data/q4.R
@@ -4,5 +4,14 @@
 
 # only use SNPs that are JUST cis or JUST trans
 only_cis <- left_join(franke_cis_data, franke_trans_data, by=c("SNP", "SNPChr", "SNPPos", "AssessedAllele", "OtherAllele"))
-fwrite(onlycis, "")
 only_trans <- right_join(franke_cis_data, franke_trans_data, by=c("SNP", "SNPChr", "SNPPos", "AssessedAllele", "OtherAllele"))
+fwrite(only_cis, "q4_output/only_cis.txt", sep="\t")
+fwrite(only_trans, "q4_output/only_trans.txt", sep="\t")
+
+only_cis <- fread("q4_output/only_cis.txt", sep="\t")
+only_cis_downsampled <- only_cis %>% sample_n(200)
+fwrite(only_cis_downsampled, "q4_output/only_cis_downsampled.txt", sep="\t")
+
+only_trans <- fread("q4_output/only_trans.txt", sep="\t")
+only_trans_downsampled <- only_trans %>% sample_n(200)
+fwrite(only_trans_downsampled, "q4_output/only_trans_downsampled.txt", sep="\t")

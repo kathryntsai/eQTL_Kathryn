f8d871eb6b4e907acd55bc1712ea6467a96db1bd
Katie <katctsai@gmail.com>
2019-07-29 12:13:23 -0400

    major updates to single cell heatmaps, proprotions, impact comparison graphs

--- kathryn/single_cell_investigation/single_cell_investigation.R
+++ kathryn/single_cell_investigation/single_cell_investigation.R
@@ -1,9 +1,10 @@
-library(ggthemes)
-theme_set(theme_stata())
 library(reshape2)
 library(dplyr)
 library(readxl)
 library(gridExtra)
+library(ggplot2)
+library(ggthemes)
+theme_set(theme_stata())
 
 sc_eqtl <- read_xlsx("/Users/kathryntsai/OneDrive\ -\ Villanova\ University/College/2018-2019/Summer\ 2019/TFs_eQTLs_Research/RProjects/Project2/IMPACT/NIHMS76345-supplement-Supplementary_table_2_vanderWijst.xlsx", skip=1)
 
@@ -150,13 +151,93 @@ gg8 <- melt(sc_eqtl,
 grid.arrange(gg1, gg2, gg3, gg4, gg5, gg6, gg7, gg8, ncol=2)
 
 
+### Making a Heat Map
+library(pheatmap)
+library(RColorBrewer)
+library(viridis)
+
+gg17 <- melt(sc_eqtl, 
+            id.vars=c("SNP", "Gene"), 
+            #measure.vars = c("PBMC__1","T CD4+__1", "T CD8+__1", "NK__1", "B__1", "DC__1", "Mono__1", "cMono__1", "ncMono__1")
+            measure.vars = c("PBMC","T CD4+", "T CD8+", "NK", "B", "DC", "Mono")
+)
+
+mat_col <- data.table(gg17$variable)
+mat_colors <- list(group = brewer.pal(length(unique(gg17$variable)), "Set1"))
+names(mat_colors$group) <- unique(gg17$variable)
+
+quantile_breaks <- function(xs, n = 10) {
+  breaks <- quantile(xs, probs = seq(0, 1, length.out = n))
+  breaks[!duplicated(breaks)]
+}
+
+mat_breaks <- quantile_breaks(mat, n = 11)
 
 
-  # gg <- mutate(sc_eqtl, my_id=paste0(SNP, Gene)) %>% melt(sc_eqtl, id.vars=c("SNP", "Gene"))
-  # melt(g, 
-  #      id.vars="variable vars",
-  #      
-  #      ) 
+draw_colnames_45 <- function (coln, gaps, ...) {
+  coord <- pheatmap:::find_coordinates(length(coln), gaps)
+  x     <- coord$coord - 0.5 * coord$size
+  res   <- grid::textGrob(
+    coln, x = x, y = unit(1, "npc") - unit(3,"bigpts"),
+    vjust = 0.75, hjust = 1, rot = 45, gp = grid::gpar(...)
+  )
+  return(res)
+}
+assignInNamespace(
+  x = "draw_colnames",
+  value = "draw_colnames_45",
+  ns = asNamespace("pheatmap")
+)
+
+pheatmap(
+  mat               = -log10(sc_eqtl[, c("PBMC","T CD4+", "T CD8+", "NK", "B", "DC", "Mono")]),
+  color             = inferno(length(mat_breaks) -1),
+  breaks            = mat_breaks,
+  border_color      = F,
+  cellwidth = 15,
+  cellheight= 3,
+  legend = TRUE,
+  show_colnames     = T,
+  show_rownames     = F,
+  treeheight_row = 0,
+  treeheight_col = 0,
+  annotation_names_col    =  data.table(group = unique(gg17$variable)),
+  annotation_legend = TRUE,
+  #annotation_names_row = data.table(group = paste0(gg17$SNP, "-", gg17$Gene)),
+  annotation_colors = mat_colors,
+  drop_levels       = TRUE,
+  fontsize          = 3,
+  main              = "Quantile Color Scale with Log Values"
+)
+
+# https://slowkow.com/notes/pheatmap-tutorial/
+  
+# library(scales)
+# library(plyr)
+# gg17 <- melt(sc_eqtl, 
+#              id.vars=c("SNP", "Gene"), 
+#              #measure.vars = c("PBMC__1","T CD4+__1", "T CD8+__1", "NK__1", "B__1", "DC__1", "Mono__1", "cMono__1", "ncMono__1")
+#              measure.vars = c("PBMC","T CD4+", "T CD8+", "NK", "B", "DC", "Mono")
+# )
+# gg17 <- ddply(gg17, .(variable), transform, rescale = rescale(-log(value)))
+# gg17$eQTL <- paste0(gg17$SNP, gg17$Gene)
+# base_size <- 9
+# gg17_plot <- ggplot(gg17, aes(variable, eQTL)) + 
+#   geom_tile(aes(fill = rescale), colour = "white") + 
+#   #scale_fill_gradient(low = "white", high = "steelblue") + 
+#   theme_grey(base_size = base_size) + 
+#   labs(x = "", y = "") + 
+#   scale_x_discrete(expand = c(0, 0)) +     
+#   scale_y_discrete(expand = c(0, 0)) + 
+#   theme_economist() + 
+#   theme(axis.text.y=element_text(size=3), 
+#         axis.text.x=element_text(size=8),
+#         axis.title=element_text(size=8,face="bold"), 
+#         legend.title = element_text(size = 3), 
+#         legend.text = element_text(size = 3)
+#        )
+# theme(legend.position = "none", axis.ticks = theme_blank(), axis.text.x = theme_text(size = base_size * 0.8, angle = 330, hjust = 0, colour = "grey50"))
+
 
 # Do we even need sc-eQTLs if we can use IMPACT to identify the cell type?
 
